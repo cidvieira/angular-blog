fd1f1eb7c4b629ca738e476fb680be136619b0c0
Cid Vieira <cidvieira99@gmail.com>
2025-04-01 11:26:56 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Title 1",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quae ullam quaerat repudiandae amet nesciunt minus, sint corporis non nemo veritatis ipsam iste et recusandae nostrum nulla! Libero, totam facere.",
+        "photoCover": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png",
+    },
+    {
+        "id": "2",
+        "title": "Title 2",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quae ullam quaerat repudiandae amet nesciunt minus, sint corporis non nemo veritatis ipsam iste et recusandae nostrum nulla! Libero, totam facere.",
+        "photoCover": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png",
+    },
+]
